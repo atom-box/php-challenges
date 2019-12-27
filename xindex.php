08ce4fbe96bc023ee75087b3da3281f9163b45d7
Evan Genest <genest.1@osu.edu>
2019-12-27 14:59:49 -0600

    Add aspirational examples for PHP templating.

---
+++ xindex.php
@@ -0,0 +1,17 @@
+<?php 
+include "includes/header.php"; 
+include "includes/footer.php"; 
+include "admin/init.php";
+
+?>
+<div class="row">
+    <!-- Blog Entries Column -->
+    <div class="col-md-8"></div>
+    <!-- Blog Sidebar Widgets Column -->
+    <div class="col-md-4"><?php include("includes/sidebar.php"); ?></div>
+</div>
+<!-- /.row -->
+
+<?php 
+include "includes/footer.php"; 
+?>
