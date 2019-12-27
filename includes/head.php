7777e30b8a7e0d5ddf826a4572cdb64d4b310cb2
Evan Genest <genest.1@osu.edu>
2019-12-27 15:39:04 -0600

    Refactor main html file into three PHP files.

---
+++ includes/head.php
@@ -0,0 +1,20 @@
+<!-- reusable: overlay style -->
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Exercises | Evan Genest</title>
+  <meta charset="utf-8"  />
+  <link rel="icon" type="image/png" href="images/dryer.png" />
+  <link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="styles/solve.css">
+</head>
+<body>
+<div class="jumbotron">
+  <h1 class="display-4">Trial by P.H.P.</h1>
+  <p class="lead">Some PHP solutions to dev challenges.</p>
+  <hr class="my-4">
+</div>
+
+
+<div class="container">
