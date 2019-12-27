dea95d9394a256b32175999e2b903afc8a031f38
Evan Genest <genest.1@osu.edu>
2019-12-27 15:00:41 -0600

    Copy BAT SOLVE 7 minimal template for displaying solved challenges.

---
+++ index.php
@@ -0,0 +1,81 @@
+<!-- reusable: overlay style -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exercises | Evan Genest</title>
+	<meta charset="utf-8"  />
+  <link rel="icon" type="image/png" href="images/dryer.png" />
+  <link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="styles/solve.css">
+</head>
+<body>
+<div class="jumbotron">
+  <h1 class="display-4">Trial by P.H.P.</h1>
+  <p class="lead">Some PHP solutions to dev challenges.</p>
+  <hr class="my-4">
+</div>
+
+
+<div class="container">
+
+<article class="card text-center">
+  <h3 class="text-left inverse" >Random 5 to 7</h3>
+
+<ul class="nav nav-tabs" id="tab1" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="challenge-tab" data-toggle="tab" href="#challenge1" role="tab" aria-controls="challenge" aria-selected="true">challenge</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="solution-tab" data-toggle="tab" href="#solution1" role="tab" aria-controls="solution" aria-selected="false">solution</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="code-tab" data-toggle="tab" href="#code1" role="tab" aria-controls="code" aria-selected="false">code</a>
+  </li>
+</ul>
+<!-- -------------------111---------------------------------- -->
+<div class="tab-content propped d-flex  align-items-center justify-content-around" id="tab1Content">
+
+ <div class="tab-pane fade show cinch active" id="challenge1" role="tabpanel" aria-labelledby="challenge-tab">
+  <p>Create a RAND7 generator that relies only on the output from a RAND5 generator.   <br /><br />Your solution should output the integers from 1 to 7, inclusive, with nearly equal frequency.</p>
+  </div>
+  <div class="tab-pane cinch fade" id="solution1" role="tabpanel" aria-labelledby="solution-tab">
+
+    null
+
+
+  </div>
+  <div class="tab-pane fade cinch" id="code1" role="tabpanel" aria-labelledby="code-tab">codetttttttty</div>
+</div>
+</article>
+
+ 
+ 
+</div><!-- end container -->
+
+
+<footer class="card">
+	<img class="card-img-top" src="images/navajo_1193x238.png" />
+	<div class="overlay">
+			<address><h6 class="text-right ">Making things in Madison, Wisconsin:       <span class="text-info"><a href="http://littlefurnace.com"> Evan Genest </a> </span> </h6></address>
+		</div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
+
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+<script src="./helpers/sevens/one.js"  ></script>
+<script src="./helpers/sevens/two.js"  ></script>
+<script src="./helpers/sevens/three.js"  ></script>
+<script src="./helpers/sevens/eisenhower.js"  ></script>
+<script src="./helpers/sevens/five.js"  ></script>
+
+</body>
+</html>
+
