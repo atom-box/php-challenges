da160f9271b61012774944742ad5e1602bb7f334
Evan Genest <genest.1@osu.edu>
2020-01-03 09:05:01 -0600

    Write top ten lines of MYSQLi OO-style connecting to DB & confirming the db name.

--- includes/bookform.php
+++ includes/bookform.php
@@ -1,23 +1,27 @@
 <h3> 
-  <?php
 
-  require_once 'includes/login.php';
+<?php
+require_once 'includes/login.php';
+$db_server = new mysqli($db_hostname, $db_username, $db_password, $db_database);
 
-
-// Create connection
-$db_server = new mysqli($db_hostname, $db_username, $db_password);
-
-// Check connection
-if ($db_server->connect_error) {
-    die("Connection failed: " . $db_server->connect_error);
+if ($db_server -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $db_server -> connect_error;
+  exit();
 }
+// Return name of current default database
+if ($result = $db_server -> query("SELECT DATABASE()")) {
+  $row = $result -> fetch_row();
+  echo "Default database is " . $row[0] . "...\n...";
+  // the above newline syntax works
+  $result -> close();
+}
+
+// Change db to "test" db
+$db_server -> select_db("test");
+
+
 echo "Connected successfully ! ! ! !";
-
-
-
-
-
-  $db_server = mysql_connect($db_hostname, $db_username, $db_password);
+$db_server -> select_db('classics');
 
   echo ' 
   <form action="" method="">
