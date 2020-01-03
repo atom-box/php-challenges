ef45da4d73e7075be31c9dd5113467d0c63d08c0
Evan Genest <genest.1@osu.edu>
2020-01-03 16:25:44 -0600

    Add a loop to display the rows gotten by SELECT * FROM CLASSICS. Leave the look messy for now.

--- includes/bookform.php
+++ includes/bookform.php
@@ -8,35 +8,70 @@ if ($db_server -> connect_errno) {
   echo "Failed to connect to MySQL: " . $db_server -> connect_error;
   exit();
 }
+echo "Connected successfully ! ! ! !";
+
+
 // Return name of current default database
 if ($result = $db_server -> query("SELECT DATABASE()")) {
   $row = $result -> fetch_row();
   echo "Default database is " . $row[0] . "...\n...";
-  // the above newline syntax works
-  $result -> close();
+  // the above newline syntax worked butnowdoesnt
+  $result -> close(); // CLOSE A RESULT? NOT HANDLE?
 }
 
-// Change db to "test" db
-$db_server -> select_db("test");
+// redundant.  comment this out.
+// $db_server -> select_db('classics');
+
+$query = "SELECT * FROM classics";
+;
+if (!$result = $db_server -> query($query)) die("Problems arose, especially ..."  );
+// TODO NOW DO SOMETHING WITH THE RESULT
+///////
+echo "hubba0";
+while($row = $result->fetch_array(MYSQLI_NUM)){
+  printf ("Author %s <br>", $row[0]);
+  printf ("Title %s <br>", $row[1]);
+  printf ("Category %s <br>", $row[2]);
+  printf ("Year %s <br>", $row[3]);
+}
 
 
-echo "Connected successfully ! ! ! !";
-$db_server -> select_db('classics');
 
-  echo ' 
-  <form action="" method="">
-  <pre>
-    Author <input type="text" name="author" />
-    Title <input type="text" name="title" />
-    Category <input type="text" name="category" />
-    Year <input type="text" name="year" />
-    ISBN <input type="text" name="isbn" />
+/* numeric array */
+$row = $result->fetch_array(MYSQLI_NUM);
+echo "hubba1";
+printf ("%s (%s)\n", $row[0], $row[1]);
+echo "hubba2";
+$row = $result->fetch_array(MYSQLI_NUM);
+printf ("%s (%s)\n", $row[0], $row[1]);
 
-  </pre>
-  </form>
-  <h3>Meh</h3>
-  <h1><i>World</i></h1>
-  <p>& Co</p>
-  ';
-  ?>
+// Free result set
+$result -> free_result();
+
+$db_server -> close();
+
+
+
+
+
+
+
+
+
+echo ' 
+<form action="" method="">
+<pre>
+  Author <input type="text" name="author" />
+  Title <input type="text" name="title" />
+  Category <input type="text" name="category" />
+  Year <input type="text" name="year" />
+  ISBN <input type="text" name="isbn" />
+
+</pre>
+</form>
+<h3>Meh</h3>
+<h1><i>World</i></h1>
+<p>& Co</p>
+';
+?>
 </h3>
