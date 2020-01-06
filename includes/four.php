2342c275c70a465b3a801ae6da96ed7a9cb48e0d
Evan Genest <genest.1@osu.edu>
2020-01-06 08:03:25 -0600

    Add an input form with radio buttons.

--- includes/four.php
+++ includes/four.php
@@ -1,15 +1,37 @@
 <article class="card text-center">
 <!---------------------Lone Tab Heading-------------------->
-  <h2 class="text-left article-title-colors" >Types</h2>
+  <h2 class="text-left article-title-colors" ><a href="https://codingbat.com/prob/p109537">Stanford U.</a> <i>Coding-Bat</i></h2>
 <ul class="nav nav-tabs"  role="tablist">
   <li class="nav-item">
-    <a class="nav-link active" id="challenge-tab" data-toggle="tab" role="tab" aria-controls="challenge" aria-selected="true"><h3>Bool</h3></a>
+    <a class="nav-link active" id="challenge-tab" data-toggle="tab" role="tab" aria-controls="challenge" aria-selected="true"><h3>Bigger Two</h3></a>
   </li>
 </ul>
 <!--------------------- Content-------------------->
 <div class="tab-content propped d-flex  align-items-center justify-content-around" >
   <div class="tab-pane fade show cinch active"  role="tabpanel" aria-labelledby="challenge-tab">
-      copper sulfur oxygen
+<code>
+Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each array. Return the array which has the largest sum. In event of a tie, return a.
+</code>
+<pre>
+Examples of input→expected output:
+biggerTwo([1, 2], [3, 4]) → [3, 4]
+biggerTwo([3, 4], [1, 2]) → [3, 4]
+biggerTwo([1, 1], [1, 2]) → [1, 2]
+</pre>
+<form>
+  <input type="radio" name="first" value="5" checked> 5<br>
+  <input type="radio" name="first" value="13"> 13<br>
+  <input type="radio" name="first" value="42"> 42 <br>
+  <input type="submit" value="Submit"> 
+</form> 
+<?php
+
+
+
+
+
+
+?>
   </div>
 </div>
 </article>
