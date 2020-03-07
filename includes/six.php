25e8a5a7692030e09a37d44349c5514f7e658404
Evan Genest <genest.1@osu.edu>
2020-03-07 15:46:27 -0600

    Put in a slider.

--- includes/six.php
+++ includes/six.php
@@ -1,85 +1,37 @@
-<?php 
-// Define a class.
-
-/*
- public function __construct(
-        OrderInterface $order,
-        SlimProductInterface $product,
-        string $sku,
-        int $requestedQty,
-        ?string $zip,
-        int $liveQty,
-        \DateTimeInterface $created
-    ) {
-        $this->order = $order;
-        $this->product = $product;
-        $this->sku = $sku;
-        $this->requestedQty = $requestedQty;
-        $this->zip = $zip;
-        $this->liveQty = $liveQty;
-@@ -77,6 +85,14 @@ public function getProduct(): SlimProductInterface
-        return $this->product;
-    }
- 
-     // @return string
- 
-    public function getSku(): string
-    {
-        return $this->sku;
-    }
-*/
-
-?>
-
 
 
 <article class="card text-center">
 <!---------------------Lone Tab Heading-------------------->
-  <h2 class="text-left article-title-colors" >Logic</h2>
+  <h2 class="text-left article-title-colors" >Digits Slider</h2>
 <ul class="nav nav-tabs"  role="tablist">
   <li class="nav-item">
-    <a class="nav-link active" id="challenge-tab" data-toggle="tab" role="tab" aria-controls="challenge" aria-selected="true"><h3>Which has a larger sum? (a, b)?  (c, d)?</h3></a>
+    <a class="nav-link active" id="challenge-tab" data-toggle="tab" role="tab" aria-controls="challenge" aria-selected="true"><h3>Rewrite!</h3></a>
   </li>
 </ul>
 <!--------------------- Content-------------------->
 <div class="tab-content propped d-flex  align-items-center justify-content-around" >
   <div class="tab-pane fade show cinch active"  role="tabpanel" aria-labelledby="challenge-tab">
-
+    <p>Slide the slider and get a revised digit-sum.</p>
+    <ol>
+      <li>Rewrite the form so it has a slider</li>
+      <li>Think out the object schema</li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ol>
 
 <form action="./index.php" method="post" class="d-flex justify-content-around ">
-  <div class="inverse framed d-flex justify-content-center" >
-    <input type="radio" name="a1" value="5" checked> 5<br>
-    <input type="radio" name="a1" value="13"> 13<br>
-    <input type="radio" name="a1" value="42"> 42 <br>
+  <div class="bluelook framed d-flex justify-content-center" >
+<div class="slidecontainer ">
+  <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+</div>
   </div>
-  <div class="inverse framed d-flex justify-content-center" >
-    <select name="a2">
-    <option value="12">12</option>
-    <option value="14">14</option>
-    <option value="16">16</option>
-    <option value="18">18</option>
-    </select><br/>
-  </div>
-  <div class="inverse framed d-flex justify-content-center" >
-    <input type="radio" name="b1" value="5" checked> 5<br>
-    <input type="radio" name="b1" value="13"> 13<br>
-    <input type="radio" name="b1" value="42"> 42 <br>
-  </div>
-  <div class="inverse framed d-flex justify-content-center" >
-    <select name="b2">
-    <option value="12">12</option>
-    <option value="14">14</option>
-    <option value="16">16</option>
-    <option value="18">18</option>
-  </select><br/>
-  </div>
-  <br />
-  <input type="submit" value="Submit"> 
+
 </form>
 
 <div  class="d-flex justify-content-around ">
     <div class="inverse framed  justify-content-center" >
-      <p>Array A is </p>
+      <p>The sum of these digits... </p>
       <?php
         $a1 = $_POST["a1"] ?? 0; // null coalescing operator, PHP7 defaults!
         $a1 = (int) $a1; 
@@ -89,18 +41,8 @@
         echo "($a1, $a2)";
       ?>
     </div>
-    <div class="inverse framed  justify-content-center" >
-      <p>Array B is </p>
-      <?php
-        $b1 = $_POST["b1"] ?? 0; // null coalescing operator, PHP7 defaults!
-        $b1 = (int) $b1; 
-        $b2 = $_POST["b2"] ?? 0; // null coalescing operator, PHP7 defaults!
-        $b2 = (int) $b2;         
-        echo "($b1, $b2)";
-      ?>
-    </div>
-    <div class="inverse framed justify-content-center" >
-      <p>The greater sum is</p>
+     <div class="inverse framed justify-content-center" >
+      <p>...is this:</p>
       <?php
 
         if (isset($_POST["a1"])){
