b57cb1a02db736d244a926cb74d8740a00b476a5
Evan Genest <genest.1@osu.edu>
2020-10-16 06:08:08 -0500

    Works.

---
+++ love6.php
@@ -0,0 +1,25 @@
+<?php
+
+// The number 6 is a truly great number. Given two int values, a and b, return true if either one is 6. Or if their sum or difference is 6. Note: the function Math.abs(num) computes the absolute value of a number.
+// love6(6, 4) → true
+// love6(4, 5) → false
+// love6(1, 5) → true
+
+function love6($a, $b){
+    if (in_array(6, [$a, $b])){
+        return 'true! '."\n";
+    } else if (6 === abs($a - $b)){
+        return 'true! '."\n";
+    } else if (6 === ($a + $b)){
+        return 'true! '."\n";
+    } else {
+        return 'false.'."\n";
+    }
+}
+
+echo(love6(6, 4)); // true
+echo(love6(4, 5)); // false
+echo(love6(1, 5)); // true
+
+
+
