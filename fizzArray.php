1537420fa4a58bc5ddfce88694a64ca3463dc6e8
Evan Genest <genest.1@osu.edu>
2020-11-01 05:33:48 -0600

    Works. Used merge not unshift. Recursive.

--- fizzArray.php
+++ fizzArray.php
@@ -1,4 +1,5 @@
 <?php
+// NOTE: syntax of unshift still strange. array_merge better today
 
 // Given a number n, create and return a new int array of length n, containing 
 // the numbers 0, 1, 2, ... n-1. The given n may be 0, in which case just 
@@ -7,21 +8,16 @@
 // so it just works. The syntax to make a new int array is: new int[desired_length]   (See also: FizzBuzz Code)
 
 function fizzArray($one_number) {
-    $many_numbers = [];
+    $many_numbers = [7, 8, 7, 6, 4, 5, 3, 6, 7, 2, 8];
     if ($one_number < 1){
-        return $many_numbers;
+        return [];
     }
-    $one_number -=1;
-    array_unshift($many_numbers, $one_number);
-    fizzArray($one_number);
+    $one_number -= 1;
+    return array_merge(fizzArray($one_number) , [$one_number]); 
     return ['never', 'see', 'this'];
 }
 
 
-echo('expect FOUR');
 print_r(fizzArray(4)); // [0, 1, 2, 3]
-echo('expect FOUR');
-print_r(fizzArray(0)); // [0]
-echo('expect FOUR');
+print_r(fizzArray(1)); // [0]
 print_r(fizzArray(10)); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-echo('expect FOUR');
