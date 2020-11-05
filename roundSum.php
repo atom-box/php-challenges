a513b22cc1c03d2422add789a32d49b349a9cdd7
Evan Genest <genest.1@osu.edu>
2020-11-05 05:37:08 -0600

    Works.

--- roundSum.php
+++ roundSum.php
@@ -1,35 +1,21 @@
 <?php
 
 // For this problem, we'll round an int value 
-// up to the next multiple of 10 if its rightmost 
-// digit is 5 or more, so 15 rounds up to 20. 
-// Alternately, round down to the previous multiple 
-// of 10 if its rightmost digit is less than 5, 
-// so 12 rounds down to 10. Given 3 ints, a b c, 
-// return the sum of their rounded values. To 
-// avoid code repetition, write a separate helper 
-// "public int round10(int num) {" and call it 3 
-//     times. Write the helper entirely below and 
-//     at the same indent level as roundSum().
+// up or down as we sum it
 
-function fiveAndUp($rawInt){
-    $tailDigit = $rawInt % 10;
-    if ( in_array($tailDigit,   [0, 1, 2, 3, 4]  ) ){
-        return false;
-    }
-    return true;
-}
 
+// TODO: This is not DRY
 function rounder(int $rawInt){
     $tailDigit = $rawInt % 10;
     if ( in_array($tailDigit,   [0, 1, 2, 3, 4]  ) ){
         $roundedInt = (int)($rawInt / 10) * 10;
         echo "Rounded int is [$roundedInt]";
         return $roundedInt;
-    }
-
-
-    $roundedInt = $rawInt;  // TODO TODO
+    } 
+    $roundedInt = (int)($rawInt / 10);
+    $roundedInt += 1;
+    $roundedInt *= 10;
+    echo "Rounded int is [$roundedInt]";
     return $roundedInt;
 }
 
