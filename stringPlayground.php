babae0265818d985ced37a4d715dba57de4df0b4
Evan Genest <genest.1@osu.edu>
2020-11-08 13:34:11 -0600

    Add more methods.

--- stringPlayground.php
+++ stringPlayground.php
@@ -20,16 +20,48 @@ $phpBreaks = 'My '. PHP_EOL . 'dog' . PHP_EOL . 'has ' . PHP_EOL . 'fleas' . PHP
 // $phpBreaks = nl2br($phpBreaks);
 echo $phpBreaks;
 
-$input_string = "Theatergoer's Guide to Shakespeare";
-echo('');
+$input_string = "ThatLazyfoxjumpedoverthed@mnfenceagain!";
+echo('substr($input_string,2)=====> ');
 echo substr($input_string,2)."\n";
-echo('');
+echo('substr($input_string,2,4)=====> ');
 echo substr($input_string,2,4)."\n";
-echo('');
+echo('substr($input_string,-2)=====> ');
 echo substr($input_string,-2)."\n";
-echo('');
+echo('substr($input_string,-2,4)=====> ');
 echo substr($input_string,-2,4)."\n";
-echo('');
+echo('substr($input_string,2,-4)=====> ');
 echo substr($input_string,2,-4)."\n";
-echo('');
+echo('substr($input_string,-4,-2)=====> ');
 echo substr($input_string,-4,-2)."\n";
+echo('---'."\n");
+$num = 42;
+$bool = false;
+$array = ['horse', 'tree'];
+echo((bool)($bool));
+
+echo "Let us ES\\CA\\PE\n";
+echo "Here is the top row: !@#\$%^&*()_+-=\n";
+
+ 
+$table_name_array = array("user","student","event","task"); 
+foreach($table_name_array as $key=>$value) { 
+$table_name = implode($table_name_array, ' SHEEP '); 
+} 
+echo "Table Name\n";
+print $table_name;  
+
+// RegEx match
+$founds = [];
+$lyrics = 'Fitter, happier, more productive
+Comfortable (not drinking too much)
+Regular exercise at the gym (3 days a week)
+Getting on better with your associate employee contemporaries
+At ease
+Eating well (no more microwave dinners and saturated fats)
+A patient, better driver
+A safer car (baby smiling in back seat)';
+$pattern = "/[a-z][a-z][a-z][a-z][a-z]/i";
+
+preg_match($pattern, $lyrics, $founds);
+echo "Found this many ". count($founds) ."\n"; 
+print_r($founds);
